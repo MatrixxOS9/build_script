d843dddf64fff955179e4f13ea0770f084f3ea4e
MatrixxOS9 <zeydanking@gmail.com>
2025-02-13 11:57:17 +0700

    test build

--- afterlife.sh
+++ afterlife.sh
@@ -13,14 +13,14 @@ rm -rf hardware/xiaomi
 rm -rf vendor/xiaomi/mojito
 rm -rf vendor/xiaomi/sm6150-common
 # Initialize ROM manifest
-repo init --depth=1 -u https://github.com/ProjectSakura/android.git -b 14 --git-lfs
+repo init --depth=1 -u https://github.com/AfterlifeOS/android_manifest.git -b 14 --git-lfs
 # repo sync
 /opt/crave/resync.sh
 # cloning DT
 # remove av
 rm -rf frameworks/av
 # device tree
-git clone https://github.com/MatrixxOS9/device_xiaomi_mojito.git --depth 1 -b sakura device/xiaomi/mojito
+git clone https://github.com/MatrixxOS9/device_xiaomi_mojito.git --depth 1 -b afterlife device/xiaomi/mojito
 git clone https://github.com/MatrixxOS9/android_device_xiaomi_sm6150-common.git --depth 1 -b mojito-universe device/xiaomi/sm6150-common
 # kernel tree
 git clone https://github.com/Kou-Yeager/kernel_xiaomi_mojito.git --depth 1 -b BORE+UCLAMP kernel/xiaomi/mojito
@@ -29,11 +29,8 @@ git clone https://gitlab.com/Sepidermn/android_vendor_xiaomi_mojito.git --depth 
 git clone https://gitlab.com/Sepidermn/android_vendor_xiaomi_sm6150-common.git --depth 1 -b 14 vendor/xiaomi/sm6150-common
 # hardware tree
 git clone https://github.com/MatrixxOS9/android_hardware_xiaomi.git --depth 1 -b mojito hardware/xiaomi
-# add modification av
-git clone https://github.com/MatrixxOS9/android_frameworks_av.git --depth 1 -b lineage-21.0 frameworks/av
 # set build environment
 . build/envsetup.sh
 # lunch
-lunch lineage_mojito-ap2a-userdebug
-# full building
-mka bacon
+goafterlife mojito
+
